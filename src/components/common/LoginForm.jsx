33b2168b0038fda11f2964653ed2c9351d9379f1
Natasha Bagde <124035587+bagdenatasha2001@users.noreply.github.com>
2025-07-29 18:40:23 +0530

    Update LoginForm.jsx

--- src/components/common/LoginForm.jsx
+++ src/components/common/LoginForm.jsx
@@ -1,24 +1,3 @@
-// import React from 'react';
-// import AutoHeading from './AutoHeading';
-// import Email from '../inputs/Email';
-// import Password from '../inputs/Password';
-// import ForgotPass from './ForgotPass';
-// import Button from './Button';
-
-// export default function LoginForm() {
-//   return (
-//     <div className="w-[35.4%] p-6 rounded-[30px] shadow-lg bg-white">
-//       <div className="w-full flex flex-col justify-start  rounded-2xl  h-full">
-//         <AutoHeading />
-//         <Email />
-//         <Password />
-//         <ForgotPass />
-//         <Button text="Login" onClick={onclick} />
-//       </div>
-//     </div>
-//   );
-// }
-
 import React from 'react';
 import AutoHeading from './AutoHeading';
 import Email from '../inputs/Email';
