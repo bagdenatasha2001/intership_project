0c3ca920b7dbab6d0e5f4b495eba0e5c3e30d74d
Natasha Bagde <124035587+bagdenatasha2001@users.noreply.github.com>
2025-08-21 12:43:24 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../Components/Header/header';
+import Header from '../components/Header/header';
 import Sideheader from '../components/sideheade/Sideheader';
 import Kpi_Container from '../components/KPI_Cards/Kpi_Container';
 import ChartContainer from '../components/charts/ChartContainer';
